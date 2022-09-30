fac7738e97e64e75a6899056c5435c80e74e1723
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-09-30 17:51:47 +0700

    abc

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -3,5 +3,6 @@ eb init udagram-api-hieunt114 --platform node.js-14 --region us-east-1
 eb use udagram-api-dev-hieunt114
 eb create udagram-api-dev-hieunt114
 
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
 
 eb deploy  udagram-api-dev-hieunt114
