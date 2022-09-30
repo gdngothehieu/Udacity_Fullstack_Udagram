34a65d8d5053812725e07d70c5c04ea6b1e3578f
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-09-30 08:46:00 +0700

    abc

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read --cache-control='max-age=0, no-cache, no-store, must-revalidate' ./www s3://udacity-dom
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://aws-bucket-udagram-hieunt114/
