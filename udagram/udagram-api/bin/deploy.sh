f743986e2af624fe1c3762359653e0ba1bc037c8
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-09-30 17:02:32 +0700

    abc

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,7 +1,7 @@
-set -x
-set -e
+cd ./www/
+eb init udagram-api-hieunt114 --platform node.js-14 --region us-east-1
+eb use udagram-api-dev-hieunt114
+eb create udagram-api-dev-hieunt114
 
 
-
-
-eb deploy udagram-api-dev-hieunt114
+eb deploy  udagram-api-dev-hieunt114
