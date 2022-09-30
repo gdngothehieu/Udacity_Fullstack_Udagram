f10be9e941542671f8c6a5700f1c700292c7329c
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-09-30 10:29:52 +0700

    abc

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: 'Udagram',
-  apiHost: 'http://udacity-env.eba-4jca4my4.eu-central-1.elasticbeanstalk.com/api/v0',
+  appName: "Udagram",
+  apiHost:
+    "http://udagram-api-dev-hieunt114.eba-4jca4my4.eu-central-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
