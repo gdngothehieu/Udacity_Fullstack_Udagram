3fdbe42de812f4378232cebb8955748a07c6cb19
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-10-02 11:56:05 +0700

    abc

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'https://udagram-main-api-dev.eba-zzk3ufa8.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-main-api-dev.eba-zzk3ufa8.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
