db2ae82bb2c0f5fb8cb70710e59df3e5f9cea474
gdngothehieu123 <ngothehieusan123@gmail.com>
2022-10-02 12:19:58 +0700

    abc

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -18,40 +18,17 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  // app.use(cors());
-  app.use(
-    cors({
-      allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "X-Access-Token",
-        "Authorization",
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Methods",
-      ],
-      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-      preflightContinue: true,
-      origin: "*",
-    })
-  );
+  app.use(cors());
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS.
-
   app.use("/api/v0/", IndexRouter);
 
-  // Root URI call
   app.get("/", async (req, res) => {
     res.send("/api/v0/");
   });
 
-  // Start the Server
   app.listen(port, () => {
     console.log(`Backend server is listening on port ${port}....`);
     console.log(`Frontent server running ${process.env.URL}`);
